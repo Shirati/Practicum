b2d971c0649c5744a2640e359f77c5d59c402e93
Shirati <117397653+Shirati@users.noreply.github.com>
2023-02-23 23:12:42 +0200

    Update child-service.service.ts

--- Client/src/services/child-service.service.ts
+++ Client/src/services/child-service.service.ts
@@ -1,23 +1,23 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import Child from 'src/Models/Child';
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { environment } from 'src/environments/environment';
+// import Child from 'src/Models/Child';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ChildServiceService {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ChildServiceService {
 
-  constructor(public http: HttpClient) { }
+//   constructor(public http: HttpClient) { }
 
-  baseRouteUrlR = `${environment.baseUrl}/Child`;
+//   baseRouteUrlR = `${environment.baseUrl}/Child`;
   
-//   GetAll() {
-//     ~(`${this.baseRouteUrlR}/Get`);
-//     return this.http.get<Child[]>(`${this.baseRouteUrlR}/Get`);
+// //   GetAll() {
+// //     ~(`${this.baseRouteUrlR}/Get`);
+// //     return this.http.get<Child[]>(`${this.baseRouteUrlR}/Get`);
+// // }
+// AddChilde(child: Child) {
+//   ~(`${this.baseRouteUrlR}/post`);
+//   return this.http.post<Child>(`${this.baseRouteUrlR}/post/?u=${Child}`, child);
 // }
-AddChilde(child: Child) {
-  ~(`${this.baseRouteUrlR}/post`);
-  return this.http.post<Child>(`${this.baseRouteUrlR}/post/?u=${Child}`, child);
-}
-}
+// }
